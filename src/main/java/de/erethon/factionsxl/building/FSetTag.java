3e4dd117da0969ff6296dcffa04e3fd5e651178f
Malfrador <malfrador@gmail.com>
2021-01-27 18:57:27 +0100

    Improve material tags

--- src/main/java/de/erethon/factionsxl/building/FSetTag.java
+++ src/main/java/de/erethon/factionsxl/building/FSetTag.java
@@ -30,16 +30,22 @@ public enum FSetTag {
     WALL(MaterialSetTag.STONE_BRICKS.getValues(), MaterialTags.COBBLESTONES.getValues(), MaterialSetTag.LOGS.getValues(), MaterialTags.SANDSTONES.getValues(), MaterialTags.RED_SANDSTONES.getValues(), MaterialSetTag.PLANKS.getValues(),
             new HashSet<>(Arrays.asList(Material.BRICKS, Material.STONE, Material.ANDESITE, Material.POLISHED_ANDESITE, Material.DIORITE, Material.POLISHED_DIORITE, Material.GRANITE, Material.POLISHED_GRANITE))),
     WINDOW(MaterialTags.GLASS.getValues(), MaterialTags.GLASS_PANES.getValues()),
+    WARMTH(MaterialSetTag.CAMPFIRES.getValues(), new HashSet<>(Arrays.asList(Material.FURNACE, Material.BLAST_FURNACE, Material.FIRE))),
+    CRAFTING(new HashSet<>(Arrays.asList(Material.CRAFTING_TABLE, Material.FLETCHING_TABLE, Material.STONECUTTER, Material.FURNACE, Material.BLAST_FURNACE, Material.CARTOGRAPHY_TABLE, Material.SMITHING_TABLE, Material.ENCHANTING_TABLE, Material.LECTERN, Material.SMOKER,
+            Material.BREWING_STAND, Material.GRINDSTONE, Material.LOOM, Material.BEACON, Material.ANVIL, Material.CHIPPED_ANVIL, Material.DAMAGED_ANVIL))),
+    MARKET(MaterialSetTag.WOOL.getValues(), MaterialSetTag.FENCES.getValues()),
     DOORS(MaterialSetTag.DOORS.getValues()),
     CROPS(MaterialSetTag.CROPS.getValues()),
     FENCES(MaterialSetTag.FENCES.getValues(),MaterialSetTag.FENCE_GATES.getValues(), MaterialSetTag.WALLS.getValues()),
+    FARMING(CROPS.getMaterials(), FENCES.getMaterials()),
+    LIGHT(MaterialTags.TORCHES.getValues(), MaterialTags.LANTERNS.getValues(), new HashSet<>(Arrays.asList(Material.GLOWSTONE, Material.REDSTONE_LAMP, Material.SEA_LANTERN, Material.END_ROD))),
+    LIGHT_AND_WARMTH(WARMTH.getMaterials(), LIGHT.getMaterials()),
     FLOWERS(MaterialSetTag.FLOWERS.getValues()),
-    RURAL_WINDOW(MaterialSetTag.WOODEN_FENCES.getValues()),
+    CHEAP_WINDOW(MaterialSetTag.WOODEN_FENCES.getValues()),
     ROOF(MaterialSetTag.STAIRS.getValues(), MaterialSetTag.SLABS.getValues()),
     FURNITURE(MaterialSetTag.STAIRS.getValues(), MaterialTags.LANTERNS.getValues(), MaterialTags.TRAPDOORS.getValues(), MaterialTags.TORCHES.getValues(), MaterialSetTag.SIGNS.getValues(), MaterialSetTag.FLOWER_POTS.getValues(), MaterialSetTag.BANNERS.getValues(),
-            MaterialSetTag.CARPETS.getValues(), MaterialSetTag.BEDS.getValues(), new HashSet<>(Arrays.asList(Material.CHEST, Material.TRAPPED_CHEST, Material.CRAFTING_TABLE, Material.FURNACE, Material.BLAST_FURNACE, Material.BARREL, Material.SMOKER,
-            Material.DISPENSER, Material.DROPPER, Material.ENCHANTING_TABLE, Material.BREWING_STAND, Material.BEACON, Material.ANVIL, Material.GRINDSTONE, Material.CARTOGRAPHY_TABLE, Material.LOOM, Material.SMITHING_TABLE, Material.LADDER, Material.STONECUTTER,
-            Material.BELL, Material.NOTE_BLOCK, Material.REDSTONE_LAMP, Material.LECTERN, Material.JUKEBOX, Material.END_ROD, Material.FLETCHING_TABLE))),
+            MaterialSetTag.CARPETS.getValues(), MaterialSetTag.BEDS.getValues(), CRAFTING.getMaterials(), new HashSet<>(Arrays.asList(Material.CHEST, Material.TRAPPED_CHEST, Material.BARREL,Material.DISPENSER, Material.DROPPER,  Material.LADDER,
+            Material.BELL, Material.NOTE_BLOCK, Material.REDSTONE_LAMP, Material.JUKEBOX, Material.END_ROD))),
     WOOD_FARM_STUFF(MaterialSetTag.SAPLINGS.getValues(), MaterialSetTag.LEAVES.getValues());
 
     Set<Material> materialSetTags = new HashSet<>();
@@ -50,7 +56,6 @@ public enum FSetTag {
             materialSetTags.addAll(set);
         }
     }
-
 
     public Set<Material> getMaterials() {
         return materialSetTags;
